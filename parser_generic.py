1f7923248bba9177e14751f5a7c2373dfb0ce64e
Barnak Ghosh <101245955+Barnakghosh07@users.noreply.github.com>
2024-03-05 20:08:13 +0530

    Add files via upload

---
+++ parser_generic.py
@@ -0,0 +1,6 @@
+class MedicalDocParser():
+    def __init__(self,text):
+        self.text = text
+
+    def parse(self):
+        pass
---
+++ parser_prescription.py
@@ -0,0 +1,12 @@
+from parser_generic import MedicalDocParser
+
+
+class PrescriptionParser(MedicalDocParser):
+    def __init__(self,text):
+        MedicalDocParser.__init__(self, text)
+
+    def parse(self):
+        pass
+
+
+if __name__ -- 'name'
